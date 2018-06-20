d573b899a36345623edacb5b027c77096ffce1e3
Triki <nassim@MacBook-Air-de-Triki.local>
2018-06-20 21:24:21 +0200

    ajout d'exos

---
+++ js/permutation.js
@@ -0,0 +1,12 @@
+var nombre1 = 5;
+var nombre2 = 3;
+
+// Tapez votre code ici (sans rien modifier d'autre !)
+
+
+var temporaire=nombre1;
+nombre1=nombre2;
+nombre2=temporaire
+
+console.log(nombre1); // Doit afficher 3
+console.log(nombre2); // Doit afficher 5
---
+++ js/tva.js
@@ -0,0 +1,3 @@
+var prix=prompt("Entrez le prix du produit: ");
+var tva=19.6/100;
+console.log("le prix TTC est: "+prix*tva);
